d729b6fcbb6853f72243bd64abc11c95442dcf21
wli <17621394682@163.com>
2022-09-02 21:52:53 +0800

    1.0.0.0

---
+++ Output/CRedis/include/RedisApi.h
@@ -0,0 +1,22 @@
+#pragma once
+
+
+
+#include "RedisSpi.h"
+
+class CRedisApi
+{
+public:
+    static CRedisApi* Create();
+    virtual ~CRedisApi();
+
+    virtual int Connect(const char* ip, int port, const char* passwd, int db) = 0;
+    virtual int RegisterSpi(CRedisSpi* spi) = 0;
+
+    virtual void* GetValue(const char* key) = 0;
+    virtual int SetValue(const char* key, void* value) = 0;
+
+    virtual int Psubscrib(const char* key) = 0;
+
+};
+
